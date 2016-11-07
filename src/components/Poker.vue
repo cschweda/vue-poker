786a1fd9ae1091640d17eb4e5d7e55e1bd238be9
cschweda <cschweda@gmail.com>
2016-11-07 13:25:29 -0600

    update

--- src/components/Poker.vue
+++ src/components/Poker.vue
@@ -7,20 +7,25 @@
         <div class="well">
 
             <div style="padding-top: 5px; padding-bottom: 5px" class="text-center">
-            <button class="btn btn-default">Shuffle</button>&nbsp;&nbsp;<button class="btn btn-default">Deal</button>
+            <button class="btn btn-default" v-on:click="shuffleUpAndDeal(DECK_API,5)">Shuffle Up and Deal</button>&nbsp;&nbsp;<button class="btn btn-default" v-on:click="draw(discards.length)">Draw {{discards.length}}</button>
+             Discards: {{discards}}
           </div>
           </div>
         </div>
 
-    <div class="text-center">
-      <div v-for="card in hand" style="float: left">
-        <span style="padding: 20px">
 
-        <img :src="'../static/img/' + card.code + '.png'" height="250">
-      </span>
-      </div>
-</div>
 
+
+        <span class="col-md-12 text-center">
+          <span v-for="(card, index) in hand" v-on:click="checkForDiscard(index)" style="display: inline-block;padding-right: 20px;">
+              <span v-if="showDiscardLabel(index)">DISCARD</span>
+            <br>
+            <img :src="localImagePath + card.code + localImageExt" height="200">
+
+
+          </span>
+
+        </span>
 
 
 <div style="clear: both"></div>
@@ -38,49 +43,71 @@ export default {
   mounted: function () {
     this.status.push('App mounted.')
     // shuffle deck
-    this.shuffle (this.DECK_API)
-    this.draw (this.DECK_API, 5)
+    this.shuffleUpAndDeal (this.DECK_API,5)
+
   },
   methods: {
-    shuffle: function (deck_api) {
-      // const DECK_API = 'https://deckofcardsapi.com/api/deck/' + id + '/'
-        const shuffle_api = deck_api + 'shuffle/?deck_count=1'
-        this.axios.get(shuffle_api).then((response) => {
-          console.log('Deck shuffed: ',response.data)
-          this.status.push('Deck shuffled.')
-        }).catch(function (error) {
-          console.log('ERROR: ',error);
-        });
-        console.log('Deck API: ', deck_api)
+    shuffleUpAndDeal: function (deck_api, draw) {
+      this.discards = [];
+      const shuffle_api = deck_api + 'draw/?count=52'
+      const CARDS_TO_START = 5
+      this.axios.get(shuffle_api).then((response) => {
+
+        this.deck = response.data
+        let msg = 'Shuffled and deck array created.'
+
+        console.log(msg)
+        this.status.push(msg);
+        this.draw (CARDS_TO_START)
+      })
+
 
       },
-      draw: function (deck_api, cardsToDraw) {
+      draw: function (draw) {
 
-        const draw_api = deck_api + 'draw/?count=' + cardsToDraw
-        this.axios.get(draw_api).then((response) => {
-          this.deck = response.data;
-          console.log('Cards drawn: ',this.deck)
-          this.status.push('Cards successfully drawn.')
-          this.hand = _.clone(this.deck.cards, true);
-          console.log(this.hand)
-        }).catch(function (error) {
-          console.log('ERROR: ',error);
-        });
+        if (this.deck.cards.length >= draw ) {
+          this.hand = this.deck.cards.slice(0,draw)
+          this.deck.cards.splice(0,draw)
+          console.log('Cards left: ',this.deck.cards.length)
+        } else {
+          console.log('Cards left: ',this.deck.cards.length)
+          console.log('No more cards to draw.')
+        }
+      },
+      checkForDiscard: function (cardIndex) {
 
+        if (_.includes(this.discards,cardIndex)) {
+          let target = this.discards.indexOf(cardIndex);
+          this.discards.splice(target, 1);
+          console.log('Removed from array')
+        } else {
+          this.discards.push(cardIndex);
+          this.discards.sort();
+          console.log('Added to array')
+        }
 
+      },
+      showDiscardLabel: function (index) {
 
-
+        if (_.includes(this.discards,index)) {
+          return true
+        } else {
+          return false
+        }
 
       }
   },
   data () {
     return {
-      DECK_API: 'https://deckofcardsapi.com/api/deck/qrq36opkjw8f/',
-      IMAGE_API: 'https://deckofcardsapi.com/static/img/',
+      DECK_API: 'https://deckofcardsapi.com/api/deck/new/',
       status: [],
       deck: [],
-      localImagePath : '../assets/img/',
-      hand: {}
+      hand: [],
+      localImagePath: '../static/img/',
+      localImageExt: '.png',
+      cardsToDraw: 5,
+      discards: [],
+
     }
   },
 
