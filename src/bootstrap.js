c2b2f0d13c721cecd8b70691b25ce84a9f96c4f5
cschweda <cschweda@gmail.com>
2016-11-08 14:02:00 -0600

    edited to add bootstrap.js

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -43,7 +43,7 @@ require('animate.css/animate.css');
  *
  * http://getbootstrap.com/
  */
-// require('bootstrap');
+require('bootstrap/dist/js/bootstrap.js');
 require('bootstrap/less/bootstrap.less');
 
 
